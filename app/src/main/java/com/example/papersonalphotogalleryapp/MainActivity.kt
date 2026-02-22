f7d96aef6b1204b4d6da11c2ac3d4762aeb21ca8
Godhonestliar <aeontoran@gmail.com>
2026-02-22 12:57:59 -0500

    i think this is right

---
+++ app/src/main/java/com/example/papersonalphotogalleryapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.papersonalphotogalleryapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var largeImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        largeImage = findViewById(R.id.img_large)
+    }
+
+    fun showLargeImage(view: View) {
+        val thumbnail = view as ImageView
+        largeImage.setImageDrawable(thumbnail.drawable)
+    }
+}
---
+++ app/src/main/java/com/example/papersonalphotogalleryapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.papersonalphotogalleryapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Hide the action bar for the splash screen
+        supportActionBar?.hide()
+
+        // Transition to MainActivity after 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
